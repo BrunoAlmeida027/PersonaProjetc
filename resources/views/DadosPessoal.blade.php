b79d919972e4a3692c7313a66d75d53bbc87b721
BrunoAlmeida027 <brunalmeid@gmail.com>
2023-07-03 17:22:47 -0300

    DadosPessoal Organizando

--- resources/views/DadosPessoal.blade.php
+++ resources/views/DadosPessoal.blade.php
@@ -16,44 +16,42 @@
                 <hr>
                 <h2> {{$dadosApi['basico']['nome']}} </h2>
             </center>
-            <p><b> Nascimento: {{$dadosApi['basico']['nascimento']}}
-                    <p class="message"> Mãe: {{$dadosApi['avancado']['mae']}}
-                    <p> Nacionalidade: {{$dadosApi['avancado']['nacionalidade']}}
-                    <p> Email: {{$dadosApi['avancado']['email_principal']}}
-                    <p> Estado Civil: {{$dadosApi['avancado']['estado_civil']}}
-                    <p> Escolaridade: {{$dadosApi['avancado']['escolaridade']}}
-                    <p> Valor Renda: <font color="green"> {{$dadosApi['avancado']['rendas'][0]['valorrenda']}} </font>
-                    <p> Empresa Pagadora: {{$dadosApi['avancado']['rendas'][0]['empresapagadora']}}
-                        <hr>
-                    <h2>Telefones :</h2>
-                    <h4>Contato Principal : </h4>
-                    <p> Nome de contato : {{$dadosApi['avancado']['telefones'][0]['nome']}}
-                    <p> Telefone: {{$dadosApi['avancado']['telefones'][0]['telefone']}}
-                    <p> Tipo: {{$dadosApi['avancado']['telefones'][0]['tipo']}}
-                    <h4>Contato Secundário : </h4>
-                    <p> Nome de contato : {{$dadosApi['avancado']['telefones'][1]['nome']}}
-                    <p> Telefone: {{$dadosApi['avancado']['telefones'][1]['telefone']}}
-                    <p> Tipo: {{$dadosApi['avancado']['telefones'][1]['tipo']}}
-                        <hr>
-                        <h2>Endereço :</h2>
-
-                        @php
-                        $cep = preg_replace("/[^0-9]/", "", $dadosApi['avancado']['enderecos'][0]['cep']);
-                        $resultado_cep = json_decode(file_get_contents("https://viacep.com.br/ws/".$cep."/json/"));
-                        @endphp
-
-                        @if (!$resultado_cep || isset($resultado_cep->erro))
-                    <p>CEP não encontrado.</p>
-                    @else
-                    <p>CEP: {{ $resultado_cep->cep }}</p>
-                    <p>Logradouro: {{ $resultado_cep->logradouro }}</p>
-                    <p>Bairro: {{ $resultado_cep->bairro }}</p>
-                    <p>Cidade: {{ $resultado_cep->localidade }}</p>
-                    <p>Estado: {{ $resultado_cep->uf }}</p>
-                    @endif
+            <b> Nascimento: {{$dadosApi['basico']['nascimento']}}
+                <br class="message"> Mãe: {{$dadosApi['avancado']['mae']}}
+                <br>Nacionalidade: {{$dadosApi['avancado']['nacionalidade']}}
+                <br>Email: {{$dadosApi['avancado']['email_principal']}}
+                <br>Estado Civil: {{$dadosApi['avancado']['estado_civil']}}
+                <br>Escolaridade: {{$dadosApi['avancado']['escolaridade']}}
+                <br>Valor Renda: <font color="green"> {{$dadosApi['avancado']['rendas'][0]['valorrenda']}} </font>
+                <br>Empresa Pagadora: {{$dadosApi['avancado']['rendas'][0]['empresapagadora']}}
+                <hr>
+                <h2>Telefones :</h2>
+                <h4>Contato Principal :</h4>
+                Nome de contato : {{$dadosApi['avancado']['telefones'][0]['nome']}}
+                <br>Telefone: {{$dadosApi['avancado']['telefones'][0]['telefone']}} |
+                Tipo: {{$dadosApi['avancado']['telefones'][0]['tipo']}}
+                <p>
+                <h4>Contato Secundário : </h4>
+                Nome de contato : {{$dadosApi['avancado']['telefones'][1]['nome']}}
+                <br>Telefone: {{$dadosApi['avancado']['telefones'][1]['telefone']}} |
+                Tipo: {{$dadosApi['avancado']['telefones'][1]['tipo']}}
+                <hr>
+                <h2>Endereço :</h2>
+                @php $cep = preg_replace("/[^0-9]/", "", $dadosApi['avancado']['enderecos'][0]['cep']);
+                $resultado_cep = json_decode(file_get_contents("https://viacep.com.br/ws/".$cep."/json/"));
+                @endphp
+                @if (!$resultado_cep || isset($resultado_cep->erro))
+                <br>CEP não encontrado.</p>
+                @else
+                CEP: {{ $resultado_cep->cep }}
+                <br>Logradouro: {{ $resultado_cep->logradouro }}
+                <br>Bairro: {{ $resultado_cep->bairro }}
+                <br>Cidade: {{ $resultado_cep->localidade }}
+                <br>Estado: {{ $resultado_cep->uf }}
+                @endif
 
 
-                    <p><a href="javascript:history.go(-1)"> Voltar para a página anterior </a></p>
+                <p><a href="javascript:history.go(-1)"> Voltar para a página anterior </a></p>
 
         </form>
     </main>
