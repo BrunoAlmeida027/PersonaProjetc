e7557cf54e903ba6f7f71c9370c7c99d87f8e881
BrunoAlmeida027 <brunalmeid@gmail.com>
2023-07-04 16:49:49 -0300

    Foto no PosBusca

--- resources/views/PosBusca.blade.php
+++ resources/views/PosBusca.blade.php
@@ -11,36 +11,40 @@
     <main>
         <h2>Cliente:</h2>
         <center>
-            <h3>{{ $dadosPosBuscaApi['avancado']['nome'] }}</h3>
-            <h3><font color="red">{{ $dadosPosBuscaApi['avancado']['cpf'] }}</font></h3>
-        </center>
-            
-        <form method="POST" action="{{ route('dadosPessoal', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
-            @csrf
-            <!-- Botão Dados Pessoais -->
-            <button type="submit" name="botao_dados_pessoais">Dados Pessoais</button>
-        </form>
+            <center> <img src="{{$dadosPosBuscaApi ['avancado']['avatar']}}" alt="Perfil" width="200" height="200">
 
-        <form method="POST" action="{{ route('dadosCNPJ', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
-            @csrf
-            <!-- Botão Dados CNPJ -->
-            <button type="submit" name="botao_cnpj">Dados CNPJ</button>
-        </form>
+                <h3>{{ $dadosPosBuscaApi['avancado']['nome'] }}</h3>
+                <h3>
+                    <font color="red">{{ $dadosPosBuscaApi['avancado']['cpf'] }}</font>
+                </h3>
+            </center>
 
-        <form method="POST" action="{{ route('veiculos', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
-            @csrf
-            <!-- Botão Veículos -->
-            <button type="submit" name="botao_veiculos">Veículos</button>
-        </form>
+            <form method="POST" action="{{ route('dadosPessoal', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
+                @csrf
+                <!-- Botão Dados Pessoais -->
+                <button type="submit" name="botao_dados_pessoais">Dados Pessoais</button>
+            </form>
 
-        <form method="POST" action="{{ route('assinaturas', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
-            @csrf
-            <!-- Botão Assinaturas -->
-            <button type="submit" name="botao_assinaturas">Assinaturas</button>
-        </form>
+            <form method="POST" action="{{ route('dadosCNPJ', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
+                @csrf
+                <!-- Botão Dados CNPJ -->
+                <button type="submit" name="botao_cnpj">Dados CNPJ</button>
+            </form>
 
-        <p><a href="javascript:history.go(-1)"> Voltar para a página anterior </a></p>
-        
+            <form method="POST" action="{{ route('veiculos', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
+                @csrf
+                <!-- Botão Veículos -->
+                <button type="submit" name="botao_veiculos">Veículos</button>
+            </form>
+
+            <form method="POST" action="{{ route('assinaturas', ['cpf' => $dadosPosBuscaApi['avancado']['cpf']]) }}">
+                @csrf
+                <!-- Botão Assinaturas -->
+                <button type="submit" name="botao_assinaturas">Assinaturas</button>
+            </form>
+
+            <p><a href="javascript:history.go(-1)"> Voltar para a página anterior </a></p>
+
 
     </main>
 </body>
