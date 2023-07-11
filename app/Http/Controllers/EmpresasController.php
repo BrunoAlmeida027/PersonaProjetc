ceaa43521b5f822264fc89d6c44d73795ce8c355
BrunoAlmeida027 <brunalmeid@gmail.com>
2023-07-11 14:43:51 -0300

    redis cache

--- app/Http/Controllers/EmpresasController.php
+++ app/Http/Controllers/EmpresasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redis;
 
 
 class EmpresasController extends Controller
@@ -12,40 +13,46 @@ class EmpresasController extends Controller
    
     public function dadosEmpresas()
     {
-      
+        $empresas = Redis::get('empresas');
 
-        $dados = Http::get('https://test.alertrack.com.br/api/test_web/profile/get')->json();
-        $empresas = [];
+        if ($empresas === null) {
+            $dados = Http::get('https://test.alertrack.com.br/api/test_web/profile/get')->json();
 
-        foreach ($dados['avancado']['sociedades'] as $socio) {
-            $cnpj = $socio['cnpjempresasocio'];
-            $resultado_cnpj = json_decode(file_get_contents("https://receitaws.com.br/v1/cnpj/" . $cnpj));
+            $empresas = [];
 
-            if (isset($resultado_cnpj->erro)) {
-                continue;
+            foreach ($dados['avancado']['sociedades'] as $socio) {
+                $cnpj = $socio['cnpjempresasocio'];
+                $resultado_cnpj = json_decode(file_get_contents("https://receitaws.com.br/v1/cnpj/" . $cnpj));
+
+                if (isset($resultado_cnpj->erro)) {
+                    continue;
+                }
+
+                $empresa = [
+                    'Empresa' => $resultado_cnpj->nome,
+                    'CNPJ' => $resultado_cnpj->cnpj,
+                    'Status' => $resultado_cnpj->status,
+                    'Tipo da Empresa' => $resultado_cnpj->tipo,
+                    'Porte' => $resultado_cnpj->porte,
+                    'Participação Sociedade' => $socio['participacaosociedade'],
+                    'Data de Abertura' => $resultado_cnpj->abertura,
+                    'Natureza Jurídica' => $resultado_cnpj->natureza_juridica,
+                    'logradouro' => $resultado_cnpj->logradouro,
+                    'Número' => $resultado_cnpj->numero,
+                    'Complemento' => $resultado_cnpj->complemento,
+                    'CEP' => $resultado_cnpj->cep,
+                    'Capital' => $resultado_cnpj->capital_social
+                ];
+
+                $empresas[] = $empresa;
             }
 
-            $empresa = [
-                'Empresa' => $resultado_cnpj->nome,
-                'CNPJ' => $resultado_cnpj->cnpj,
-                'Status' => $resultado_cnpj->status,
-                'Tipo da Empresa' => $resultado_cnpj->tipo,
-                'Porte' => $resultado_cnpj->porte,
-                'Participação Sociedade' => $socio['participacaosociedade'],
-                'Data de Abertura' => $resultado_cnpj->abertura,
-                'Natureza Jurídica' => $resultado_cnpj->natureza_juridica,
-                'logradouro' => $resultado_cnpj->logradouro,
-                'Número' => $resultado_cnpj->numero,
-                'Complemento' => $resultado_cnpj->complemento,
-                'CEP' => $resultado_cnpj->cep,
-                'Capital' => $resultado_cnpj->capital_social
-            ];
-
-            $empresas[] = $empresa;
+            Redis::setex('empresas', 180, json_encode($empresas));
+            // Armazena os dados das empresas no Redis por 2 minutos
+        } else {
+            $empresas = json_decode($empresas, true);
         }
 
         return view('Empresas', ['empresas' => $empresas]);
     }
-
-  
 }
